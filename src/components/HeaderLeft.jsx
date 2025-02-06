67d3c0d4d4333e12e7c208a884708aff9eea3d06
Cole W. <coleworldsports@gmail.com>
2025-02-06 18:14:35 -0500

    added icon/sections in HeaderLeft 2/6/25

--- src/components/HeaderLeft.jsx
+++ src/components/HeaderLeft.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import image from '/public/IMG_2968.jpg' // importing my headshot
-import { BiMoon, BiSun } from 'react-icons/bi'
+import { BiMoon, BiSun, BiCurrentLocation, BiEnvelope, BiLogoGithub, BiLogoLinkedin, BiLogoTwitter} from 'react-icons/bi'
 const HeaderLeft = ({darkMode, toggleDarkMode}) => {
     return (
         // HeaderLeft takes 25% of viewport
@@ -34,9 +34,93 @@ const HeaderLeft = ({darkMode, toggleDarkMode}) => {
                         )
                     }
                  </button>
-                </div>  
+                </div> 
+                 <div className="flex flex-col items-start justify-start gap-5 border-b p-5 
+                 drak:border-gray-700">
+
+                    <ul className="space-y-3">
+                       <li className="flex-items-center gap-2">
+                        
+                            <BiCurrentLocation className="text-xl"/> 
+                            <span>Boston, MA</span>
+                        </li> 
+
+                        <li className="flex-items-center gap-2">
+                        
+                        <BiEnvelope className="text-xl"/> 
+                        <span>juliantcoleman1@gmail.com</span>
+                    </li> 
+                    </ul>
+                 </div>
+
+                    <div className="flex flex-col items-start justify-start gap-5 border-b p-5
+                    dark:border-gray-700">
+
+                    <h1 className="text-baes font-semibold 
+                    md:text-2x1">SOCIAL MEDIA</h1>
+
+                <ul className="space-y-3">
+
+                <li className="flex-items-center gap-2">
+                        
+                        <BiLogoGithub className="text-xl"/> 
+                        <span><a href="https://github.com/JColeman1550">
+                        @JColeman1550</a></span>
+                    </li> 
+                
+                    <li className="flex-items-center gap-2">
+                        
+                        <BiLogoLinkedin className="text-xl"/> 
+                        <span><a href="https://www.linkedin.com/in/juliantcoleman/">
+                        juliantcoleman</a></span>
+                    </li> 
+
+                    <li className="flex-items-center gap-2">
+                        
+                        <BiLogoTwitter className="text-xl"/> 
+                        <span><a href="https://x.com/Julian__TD">
+                        Julian__TD</a></span>
+                    </li>
+                </ul>
             </div>
-         </div> 
+
+<div className="flex flex-col items-start justoify-start
+    gap-5 border-b p-5 dark: border-gray-700">
+
+<h1 className="text-baes font-semibold 
+md:text-2x1">EDUCATION</h1>
+
+
+<ul>
+
+<li className="relative flex flex-col gap-1 border-l p-6 before:absolute before:left-[-6px] 
+before:top-1/2 before:h-3 before:w-3 
+before: -translate-y1/2 before:rounded-full 
+before:bg-black dark:before:bg-white before:transform">
+    <span className="font-semibold md:text-lg">Political Science</span>
+    <span className="font-light">University of Southern California</span>
+    <span className="text-sm text-gray-400">2012-2014</span>
+</li>
+
+<li className="relative flex flex-col gap-1 border-l p-6 before:absolute before:left-[-6px] 
+before:top-1/2 before:h-3 before:w-3 
+before: -translate-y1/2 before:rounded-full 
+before:bg-black dark:before:bg-white before:transform">
+    <span className="font-semibold md:text-lg">Certified Entry-Level Python Programmer</span>
+    <span className="font-light">Python Institute</span>
+    <span className="text-sm text-gray-400">2024</span>
+</li>
+
+<li></li>
+
+
+</ul>
+
+
+    </div>
+
+        </div>
+    </div> 
     )
 
 }

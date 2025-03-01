d3f56f0ef318bec6de0310e1a0ad6ba6b94f7db2
Julian Coleman <coleworldsports@gmail.com>
2025-02-28 20:19:05 -0500

    added img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,7 +38,7 @@ before: -translate-y1/2 before:rounded-full
 before:bg-black dark:before:bg-white before:transform"
               >
                 <span className="text-lg font-semibold">
-               <a href="https://julianc.vercel.app/">Personal Web Development Blog</a>   
+               <a href="https://github.com/JColeman1550/portfolio-1/blob/main/public/home-screen.png?raw=true">Personal Web Development Blog</a>   
                 </span>
 
                 <span className="font-light">
